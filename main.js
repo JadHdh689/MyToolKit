070c30a9cf32fdfec160996d473918ce05674db5
Jad <jadhdeife345@gmail.com>
2025-06-17 00:09:04 +0300

    trying public

--- main.js
+++ main.js
@@ -4,12 +4,12 @@ const booksData = {
     {
       id: '1.1',
       title: 'Just. Be. Calm',
-      cover: 'images/group1/book1_1/cover1_1.jpg',
+      cover: 'public/images/group1/book1_1/cover1_1.jpg',
       description: 'About1.1.txt',
       assets: [
-        { name: 'Story', path: 'images/group1/book1_1/story1_1.png' },
-        { name: 'Guide', path: 'images/group1/book1_1/guide1_1.png' },
-        { name: 'Activity', path: 'images/Group 1/Book1.1/Activity1.1.png' },
+        { name: 'Story', path: 'public/images/group1/book1_1/story1_1.png' },
+        { name: 'Guide', path: 'public/images/group1/book1_1/guide1_1.png' },
+        { name: 'Activity', path: 'public/images/Group 1/Book1.1/Activity1.1.png' },
         { name: 'Stickers', path: 'images/Group 1/Book1.1/Stickers1.1.png' },
         { name: 'Certificate', path: 'images/Group 1/Book1.1/Certificate1.1.png' }
       ]
